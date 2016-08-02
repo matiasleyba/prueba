ce7869e44c500afa06161db37d5646cb7768cf96
matiasleyba <matileyba@gmail.com>
2016-08-02 19:41:42 -0300

    prueba1

--- utils.rb
+++ utils.rb
@@ -1,9 +1,6 @@
 module Utils
   def self.say_hello_to(name,language)
-    if language == "es"
-      puts "hola #{@name}!"
-    else
-      puts "no entiendo tu idioma"
-    end
+    language == "es" ? puts "hola #{@name}!" : puts "no comprendo tu idioma"
+    
   end
 end
